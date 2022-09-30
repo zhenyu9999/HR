f6c1d635c4fc15a629a8ccfca5d0351a4147a670
zhenyu9999 <112604982+zhenyu9999@users.noreply.github.com>
2022-09-30 23:28:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def addEmployee():
         s3 = boto3.resource('s3',
                             aws_access_key_id = 'ASIAU56IROEJZ74NATT3',
                             aws_secret_access_key = 'FNdBkp1T6c1II/SMxgs9Zde6dCfcR1h5bWcGENhz',
-                            aws_security_token = 'FwoGZXIvYXdzELn//////////wEaDK1UsEM85JZfNu1HUiLQAUaRuFzRhFSfml7uyJUnm1RzmeX890BDAH9SQ72bVI68lUGgvuNC$')
+                            aws_session_token = 'FwoGZXIvYXdzELn//////////wEaDK1UsEM85JZfNu1HUiLQAUaRuFzRhFSfml7uyJUnm1RzmeX890BDAH9SQ72bVI68lUGgvuNC$')
                             # aws_access_key_id='ASIAU56IROEJTPKIIEXE',
                             # aws_secret_access_key='9wa0cplmvAdaG82JOjMbHVGqvFLpUCgm1yonp+kt')
 
